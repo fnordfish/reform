372b04f3121d96414d9e72f64a45a366f56cec74
Robert Schulze <robert@dotless.de>
2016-03-21 14:31:51 +0100

    Update to dry-validation 0.7
    Same Group inheritance is not yet working.

--- lib/reform/form/dry.rb
+++ lib/reform/form/dry.rb
@@ -20,20 +20,36 @@ module Reform::Form::Dry
     end
 
     class Group
-      def initialize
-        @validator = Class.new(ValidatorSchema)
+      def initialize(options = {})
+        base = options.fetch(:base, ValidatorSchema)
+        dsl_opts = {
+          schema_class: Class.new(base.is_a?(Dry::Validation::Schema) ? base.class : base),
+          parent: options[:parent]
+        }
+
+        @dsl = Dry::Validation::Schema::Value.new(dsl_opts)
       end
 
-      def instance_exec(&block)
-        @validator.class_eval(&block)
+      def instance_exec(options = {}, &block)
+        dsl = @dsl
+        dsl.instance_exec(&block)
+
+        klass = dsl.schema_class
+        klass.configure do |config|
+          config.rules = config.rules + (options.fetch(:rules, []) + dsl.rules)
+          config.checks = config.checks + dsl.checks
+          config.path = dsl.path
+        end
+
+        @validator = klass.new
       end
 
       def call(fields, reform_errors, form)
-        validator = @validator.new(form)
+        validator = @validator.with(form: form)
 
-        # a message item looks like: {:confirm_password=>[["confirm_password size cannot be less than 2"], "9"]}
+        # a result looks like: output={:confirm_password=>"9"} messages={:confirm_password=>["size cannot be less than 2"]}
         validator.call(fields).messages.each do |field, dry_error|
-          dry_error[0].each do |attr_error|
+          dry_error.each do |attr_error|
             reform_errors.add(field, attr_error)
           end
         end
@@ -41,9 +57,9 @@ module Reform::Form::Dry
     end
 
     class ValidatorSchema < Dry::Validation::Schema::Form
-      def initialize(form)
-        @form = form
-        super()
+      def initialize(rules, options)
+        @form = options[:form]
+        super
       end
 
       def form
